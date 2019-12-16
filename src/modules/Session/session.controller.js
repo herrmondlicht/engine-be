7e7f0a379eb494353b8d132bfa3be41160166bcb
Guilherme Moreira <guilherme.silva@yobetit.com>
2019-12-16 01:37:51 +0100

    feat(session): create login
    - add login route
    - add login controller

---
+++ src/modules/Session/session.controller.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+export default () => ({
+  login: (req, res) => {
+    const response = authenticateUser(req.body);
+    if (response) return res.status(200).json(response);
+    return res
+      .status(401)
+      .json({ status: 401, errorText: "Invalid username or password" });
+  }
+});
+
+const authenticateUser = ({ username, password }) => {
+  const { USERNAME, PASSWORD, SECRET } = process.env;
+  if (username === USERNAME && password === PASSWORD) {
+    const token = jwt.sign(
+      {
+        role: "admin"
+      },
+      SECRET,
+      { expiresIn: "24h" }
+    );
+    return { token };
+  }
+};
---
+++ src/modules/Session/session.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import SessionController from "./session.controller";
+const router = express.Router();
+
+const sessionController = SessionController();
+router.post("/login", sessionController.login);
+
+export default router;
