a62dd0b645ac621add7dd99340a762e463a2404d
Guilherme Moreira <guilherme.silva@yobetit.com>
2020-01-07 01:32:08 +0100

    chore(package.json): add cors package

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
-import appRoutes from './index.routes'
-
+import appRoutes from "./index.routes";
+import cors from "cors";
 const { PORT } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json({limit: '1mb'}))
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
+app.use(cors());
+app.use(bodyParser.json({ limit: "1mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 //load routes
 app.use("/api", appRoutes);
 
-app.use(function errorHandler (err, req, res, next) {
-  console.log(err)
+app.use(function errorHandler(err, req, res, next) {
+  console.log(err);
   res.status(500).json({
-    "status":500,
-    "message":"something went wrong"
-  })
-})
-
+    status: 500,
+    message: "something went wrong"
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is now running on port ${PORT}`);
