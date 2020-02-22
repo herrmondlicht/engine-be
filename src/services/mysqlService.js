75754ba3d786db2a29d7a3a959a34bfa40c4236c
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-02-22 22:04:29 +0100

    feat(mysqlService): add service for mysql

---
+++ src/services/mysqlService.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+import mysql from "mysql";
+const {DB_PASSWORD, DB_HOST} = process.env;
+
+const _dbconfig = {
+	host: DB_HOST,
+  password: DB_PASSWORD,
+  database: "engine",
+  user: "root",
+  port: 3307
+}
+
+
+function connectionWithTransaction(){
+	const connection = connectToDB();
+	return new Promise((resolve,reject) => {
+		connection.beginTransaction((err) => {
+			try{
+				if(err){
+					return reject(err)
+				}
+				return resolve(connection);
+			}
+			catch(e){
+				return reject(e)
+			}
+		})
+	})
+}
+
+function querySQLWithConnection(connection) {
+	return (query, options) =>
+		new Promise((resolve, reject) => {
+			try{
+				connection.query(query, options, (err, results, fields) => {
+					if (err) return reject(err);
+					else return resolve(results);
+				});
+			}
+			catch(e){
+				reject(e)
+			}
+		});
+}
+
+async function query(query, options) {
+	try {
+		const connection = connectToDB();
+		const result = await querySQLWithConnection(connection)(query, options);
+		connection.end();
+		return result;
+	} catch (e) {
+		console.log(e);
+		throw e;
+	}
+}
+
+function connectToDB(dbconfig = _dbconfig) {
+	const connection = mysql.createConnection({
+		...dbconfig,
+		multipleStatements: true
+	});
+	connection.connect();
+	return connection;
+}
+
+export default () => ({
+	query,
+	connectionWithTransaction
+})
