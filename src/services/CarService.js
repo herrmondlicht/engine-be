295f5dc800a29ac7afa245753cc2e195768fa6a7
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-02-25 23:56:44 +0100

    feat(CarService): add method to add cars

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -1,13 +1,24 @@
+import queryHelperService from "./queryHelperService";
 import MySqlService from "./mysqlService";
 
-export default ({ mysqlService } = { mysqlService: MySqlService() }) => {
-  const getList = async ({ fields, limit } = {}) => {
-    const columns = fields ? fields.split(",") : "*"
-    const carList = await mysqlService.query(`SELECT ?? from cars`, [columns])
+export default ({ queryService = queryHelperService(), mysqlService = MySqlService() } = {}) => {
+  const getList = async ({ fields, limit, query } = {}) => {
+    const carList = await queryService.getFrom("cars", { fields, limit, query })
     return carList;
   }
 
+  const addCar = async (data, { connection = mysqlService.connectToDB() } = {}) => {
+    const resultFromInsert = await mysqlService.querySQLWithConnection(connection)(`
+        INSERT INTO cars SET ?
+        ON DUPLICATE KEY UPDATE
+        ?;
+    `, [data, data])
+    const selectFromInsert = await queryService.getFrom("cars", { query: JSON.stringify(data) })
+    return selectFromInsert
+  }
+
   return {
-    getList
+    getList,
+    addCar
   }
 }
