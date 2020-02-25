7c3c25bcb325ae4f90b4d3686a3b4572454cf075
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-02-26 00:23:23 +0100

    fix(queryHelperService):  makeWhereQuery
    - removes the need to parse json to string before reaching the function

--- src/services/queryHelperService.js
+++ src/services/queryHelperService.js
@@ -5,46 +5,44 @@ import MySqlService from "./mysqlService";
 
 const getFrom = (mysqlService) =>
   async (table, { fields, limit, query } = {}) => {
-    try{
+    try {
       const columns = fields ? fields.split(",") : "*";
-      const whereClause = query? makeWhereQuery(query) : ""
+      const whereClause = query ? makeWhereQuery(query) : ""
       const retrievedData = await mysqlService.query(`SELECT ?? from ?? ${whereClause}`, [columns, table])
       return retrievedData;
     }
-    catch(e){
-      if(e.code === "ER_BAD_FIELD_ERROR"){
+    catch (e) {
+      if (e.code === "ER_BAD_FIELD_ERROR") {
         throw {
-          message:"field declared in query was not found"
+          message: "field declared in query was not found"
         }
       }
     }
-    
+
   }
 
 const makeWhereQuery = (query) => {
-  try{
-    console.log(query)
-    const JSONQuery = JSON.parse(query)
-    console.log(JSONQuery)
-    return Object.entries(JSONQuery).reduce((prev,[key, value], index) => {
-      if(index === 0){
-        return mysql.format("WHERE ?? = ?", [key,value]);
+  try {
+    const JSONQuery = typeof query === "string" ? JSON.parse(query) : query
+    return Object.entries(JSONQuery).reduce((prev, [key, value], index) => {
+      if (index === 0) {
+        return mysql.format("WHERE ?? = ?", [key, value]);
       }
-    
-      return `${prev} ${mysql.format("AND ?? = ?", [key,value])}`
-    
+
+      return `${prev} ${mysql.format("AND ?? = ?", [key, value])}`
+
     }, "")
   }
-  catch(e){
+  catch (e) {
     console.error(e, "Could not parse query coming from other service")
     throw {
-      message:"Could not parse query, please verify the query sent to the endpoint"
+      message: "Could not parse query, please verify the query sent to the endpoint"
     };
   }
 
 }
 
-export default ({ mysqlService } = { mysqlService: MySqlService() }) => {
+export default ({ mysqlService = MySqlService() } = {}) => {
   return {
     getFrom: getFrom(mysqlService)
   }
