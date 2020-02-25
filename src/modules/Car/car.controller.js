5bdde858496bc7e4d6ee2a06a40c14b9688ae003
Guilherme Moreira da Silva <guilherme@Mias-MacBook-Pro.local>
2020-02-26 00:26:39 +0100

    feat(car.controller): add create and better error handling
    - add create method
    - add respondError to better handle error messages

--- src/modules/Car/car.controller.js
+++ src/modules/Car/car.controller.js
@@ -2,25 +2,54 @@
 export default ({ carService }) => ({
   list: async (req, res, next) => {
     try {
-      const carList = await carService.getList({...req.query});
+      const carList = await carService.getList({ ...req.query });
       res.status(200).json({
         data: carList
       })
     }
     catch (e) {
-      res.status(500).json({
-        status:500,
-        message: "something went wrong"
-      })
+      return respondError(res, e)
     }
   },
-  create: (req, res) => {
-    res.status(200).json({
-      created: true,
-      message: "grrrrreat job"
-    })
+  byId: async (req, res, next) => {
+    try {
+      const carList = await carService.getList({ query: { id: req.params.id } });
+      if (carList.length)
+        res.status(200).json({
+          data: carList[0]
+        })
+      else
+        res.status(404).json({
+          message: "resource was not found"
+        })
+    }
+    catch (e) {
+      return respondError(res, e)
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const { body } = req;
+      const car = await carService.addCar(body)
+      res.status(200).json({
+        created: true,
+        data: car
+      })
+    }
+    catch (e) {
+      return respondError(res, e)
+    }
   },
   update: (req, res) => {
 
   }
 });
+
+
+
+const respondError = (res, e) => {
+  return res.status(500).json({
+    status: 500,
+    message: e.message || "something went wrong"
+  })
+}
