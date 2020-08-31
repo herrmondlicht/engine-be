e3d5eb588f22b23c460e3c04467961325761bb85
Guilherme Moreira <guilhermegms.silva@gmail.com>
2020-08-31 21:54:16 +0200

    fix(queryBuilder): remove old credentials

--- src/services/databaseService/queryBuilder.js
+++ src/services/databaseService/queryBuilder.js
@@ -1,12 +1,10 @@
 import knex from 'knex';
 
-import { DATABASE_CONFIG } from '../../constants/sqlCredentials';
+import { DATABASE_URL } from '../../constants/sqlCredentials';
 
 export default knex({
   client: 'mysql',
-  connection: {
-    ...DATABASE_CONFIG,
-  },
+  connection: DATABASE_URL,
   pool: {
     min: 2,
     max: 10,
