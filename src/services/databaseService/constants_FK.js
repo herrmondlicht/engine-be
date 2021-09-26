bbbe0fb47f0bba7a8e97cb58c996042578c26734
Guilherme Silva <guilherme.silva@redacreltd.com>
2021-09-26 20:37:21 +0200

    feat(serviceOrderService): add sum of service items

--- src/services/databaseService/constants_FK.js
+++ src/services/databaseService/constants_FK.js
@@ -2,4 +2,5 @@ export default {
   cars: 'car_id',
   customers: 'customer_id',
   customer_cars: 'customer_car_id',
+  service_orders: 'service_order_id',
 };
--- src/services/serviceOrderService.js
+++ src/services/serviceOrderService.js
@@ -1,5 +1,25 @@
+import FK_CONSTANTS from './databaseService/constants_FK';
 import _commonService from './commonService';
+import _queryService from './databaseService/whereQueryHelperService';
+import queryBuilder from './databaseService/queryBuilder';
 
-export default ({ commonService = _commonService({ resourceName: 'service_orders' }) } = {}) => ({
+const sumServiceItems = (builderQuery) => {
+  builderQuery.join('service_order_items', `service_orders.id`, '=', `service_order_items.${FK_CONSTANTS.service_orders}`).sum({
+    service_items_price: queryBuilder.raw('?? ?? ??', ['service_order_items.unit_price', '*', 'service_order_items.quantity']),
+  });
+};
+
+export default ({ commonService = _commonService({ resourceName: 'service_orders' }), queryService = _queryService({ queryBuilder }) } = {}) => ({
   ...commonService,
+  getList: async ({ limit, q, include, resourcesJoinIds } = {}) => {
+    const resourceList = queryService.getFrom('service_orders', {
+      fields: 'service_orders.*',
+      limit,
+      query: q,
+      include,
+      resourcesJoinIds,
+      customQueryBuilderOperation: sumServiceItems,
+    });
+    return resourceList;
+  },
 });
