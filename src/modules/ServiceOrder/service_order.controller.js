3daa50e3d3de840cb544a339b68f675e712b77d7
Guilherme Silva <guilhermegms.silva@gmail.com>
2023-06-18 08:24:56 -0300

    fix(service_report): wrong 200 response format being returned

--- src/modules/ServiceOrder/service_order.controller.js
+++ src/modules/ServiceOrder/service_order.controller.js
@@ -1,15 +1,17 @@
-const getReports = ({serviceReportService, queryBuilder}) => async (req,res, next) => {
-  try{
-    const calculations = await serviceReportService.getServicePriceCalculation(queryBuilder)
-    res.status(200).json(calculations)
-  } catch(e){
-    console.log(e)
-    next(e);
-  }
+const getReports =
+  ({ serviceReportService, queryBuilder }) =>
+  async (req, res, next) => {
+    try {
+      const calculations = await serviceReportService.getServicePriceCalculation(queryBuilder);
+      res.status(200).json({
+        data: calculations,
+      });
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  };
 
-}
-
-
-export default ({serviceReportService, queryBuilder}) => ({
-  getServicePriceCalculation: getReports({serviceReportService, queryBuilder})
-})
+export default ({ serviceReportService, queryBuilder }) => ({
+  getServicePriceCalculation: getReports({ serviceReportService, queryBuilder }),
+});
