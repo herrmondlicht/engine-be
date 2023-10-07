43c3d362d5415cf71724f97f650223fec4cba791
Guilherme Silva <guilhermegms.silva@gmail.com>
2023-10-07 04:20:04 -0300

    Feat/reports (#54)
    * feat: add endpoint to handle reports
    
    * fix(service_report): wrong 200 response format being returned
    
    * feat: update service order item triggers update to service order price
    
    * fix(service_orders): remove unnecessary code for sum price for

--- src/services/serviceOrderService.js
+++ src/services/serviceOrderService.js
@@ -1,24 +1,24 @@
 export default ({ commonService, queryService, queryBuilder } = {}) => ({
   ...commonService,
-  getList: async ({ limit, q, include, resourcesJoinIds } = {}) => {
-    const includeSelect =
-      include
-        ?.split(',')
-        ?.map((includeItem) => `${includeItem}.*`)
-        ?.join(',') || '';
-    const resourceList = queryService.getFrom('service_orders', {
-      fields: [
-        queryBuilder.raw('*'),
-        queryBuilder.raw(
-          '(select SUM(unit_price *  quantity) from service_order_items where service_order_id=service_orders.id and deleted_at is null) as service_items_price'
-        ),
-        ...(includeSelect ? [queryBuilder.raw(includeSelect)] : []),
-      ],
-      limit,
-      query: q,
-      include,
-      resourcesJoinIds,
-    });
-    return resourceList;
-  },
+  // getList: async ({ limit, q, include, resourcesJoinIds } = {}) => {
+  //   const includeSelect =
+  //     include
+  //       ?.split(',')
+  //       ?.map((includeItem) => `${includeItem}.*`)
+  //       ?.join(',') || '';
+  //   const resourceList = queryService.getFrom('service_orders', {
+  //     fields: [
+  //       queryBuilder.raw('*'),
+  //       queryBuilder.raw(
+  //         '(select SUM(unit_price *  quantity) from service_order_items where service_order_id=service_orders.id and deleted_at is null) as service_items_price'
+  //       ),
+  //       ...(includeSelect ? [queryBuilder.raw(includeSelect)] : []),
+  //     ],
+  //     limit,
+  //     query: q,
+  //     include,
+  //     resourcesJoinIds,
+  //   });
+  //   return resourceList;
+  // },
 });
