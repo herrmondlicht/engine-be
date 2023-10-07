618bba5f068aa22cc86d5f4426617b1b5aa8a524
Guilherme Silva <guilhermegms.silva@gmail.com>
2023-10-07 04:10:12 -0300

    fix(service_orders): remove unnecessary code for sum price for

--- src/modules/ServiceOrder/service_order.routes.js
+++ src/modules/ServiceOrder/service_order.routes.js
@@ -1,30 +1,27 @@
 import express from 'express';
 
 import bindResourcesToRoute from '../../middlewares/bindResourcesToRoute';
-import makeServiceOrderService from '../../services/serviceOrderService';
 import serviceOrderItemRouter from '../ServiceOrderItem/service_order_item.routes';
 import makeCRUDService from '../../services/makeCRUDService';
 import makeServiceReportService from '../../services/serviceReport';
 import makeCRUDController from '../Common/common.controller';
 import { queryService } from '../../services/databaseService/queryService';
 import queryBuilder from '../../services/databaseService/dbOperations/queryBuilder';
-import makeServiceOrderController from "./service_order.controller"
+import makeServiceOrderController from './service_order.controller';
 
 const router = express.Router();
 
 const CRUDService = makeCRUDService({ queryService, resourceName: 'service_orders' });
-const serviceOrderService = makeServiceOrderService({ commonService: CRUDService, queryBuilder, queryService });
-const CRUDControllerMethods = makeCRUDController({ resourceService: serviceOrderService });
-const serviceReportService = makeServiceReportService({queryBuilder});
-const serviceOrderController = makeServiceOrderController({serviceReportService ,queryBuilder});
+const CRUDControllerMethods = makeCRUDController({ resourceService: CRUDService });
+const serviceReportService = makeServiceReportService({ queryBuilder });
+const serviceOrderController = makeServiceOrderController({ serviceReportService, queryBuilder });
 
 router.get('/', CRUDControllerMethods.list);
 router.post('/', CRUDControllerMethods.create);
-router.get("/reports", serviceOrderController.getServicePriceCalculation)
+router.get('/reports', serviceOrderController.getServicePriceCalculation);
 router.patch('/:id', CRUDControllerMethods.update);
 router.get('/:id', CRUDControllerMethods.byId);
 router.delete('/:id', CRUDControllerMethods.delete);
-
 
 router.use('/:id/items', bindResourcesToRoute('service_order_id', { cleanPreviousResources: true }), serviceOrderItemRouter);
 
