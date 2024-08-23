67ab10b9e742d8ec3eaf81ad6529d7c8ce609eb5
Guilherme Silva <guilhermegms.silva@gmail.com>
2024-08-23 01:14:13 -0300

    fix set-db

--- database_structure/docker-creation.sh
+++ database_structure/docker-creation.sh
@@ -3,8 +3,10 @@
 processor=$(uname -p)
 
 if [[ $processor == "arm" || $processor == "arm64" ]]; then
-   docker run --platform linux/amd64 -d -p 3307:3306 --name engine-mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
+   docker network create engine-network
+   docker run --platform linux/amd64 -d -p 3307:3306 --name engine-mysql --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
 
 else
-     docker run -d -p 3307:3306 --name engine-mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
+   docker network create engine-network
+   docker run -d -p 3307:3306 --name engine-mysql  --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
 fi
