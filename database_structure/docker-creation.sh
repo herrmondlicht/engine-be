3005b97991d4d4965d62e6a143e819de5095bfb1
Guilherme Silva <guilhermegms.silva@gmail.com>
2024-12-10 21:26:45 -0300

    chore(docker): add networking to docker

--- database_structure/docker-creation.sh
+++ database_structure/docker-creation.sh
@@ -4,9 +4,9 @@ processor=$(uname -p)
 
 if [[ $processor == "arm" || $processor == "arm64" ]]; then
    docker network create engine-network
-   docker run --platform linux/amd64 -d -p 3307:3306 --name engine-mysql --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
+   docker run --platform linux/amd64 -d -p 3307:3306 --name engine-mysql --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql -v ~/data/engine_data:/var/lib/mysql mysql:5.7
 
 else
    docker network create engine-network
-   docker run -d -p 3307:3306 --name engine-mysql  --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql mysql:5.7
+   docker run -d -p 3307:3306 --name engine-mysql  --network engine-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true -v $(pwd)/database_structure/db_structure.sql:/docker-entrypoint-initdb.d/01.sql -v ~/data/engine_data:/var/lib/mysql mysql:5.7
 fi
